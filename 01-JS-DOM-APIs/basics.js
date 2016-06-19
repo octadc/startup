257c456a5f3881b125efaa48233116471468aed9
OctavioDellavedova <octadc@gmail.com>
2016-06-19 20:27:54 -0300

    repository list

--- 01-JS-DOM-APIs/basics.js
+++ 01-JS-DOM-APIs/basics.js
@@ -2,3 +2,25 @@ $(document).ready(function(){
 	$("#hid").fadeIn(1200);
 })
 
+function ajaxPromise(url, method, params){
+		var p = new Promise(function(resolve,reject) {
+			var xhttp = new XMLHttpRequest();
+    		xhttp.open(method, url+"?"+params, true);
+			xhttp.onload = function () {
+          		if (this.status == 200 && this.response.length > 0) {
+          			resolve(this.response)
+	          } else {
+	           		reject({type:'Error'});
+	          }
+        	};
+        	
+        	xhttp.onerror = function () {
+          		reject({type:'Error'});
+    		};
+    		xhttp.send();
+		})
+		
+		return p;
+	}
+
+
