bac4b89b740e6c8d2f5b4423968474ec3dcc027c
OctavioDellavedova <octadc@gmail.com>
2016-06-20 12:02:50 -0300

    Creating movie class

---
+++ 02-oop-inheritance/class.js
@@ -0,0 +1,16 @@
+class Movie {
+	constructor(title, year, duration) {
+	this.title = title;
+	this.year = year;
+	this.duration = duration;
+	}
+	play(){
+		console.log("Playing");
+	}
+	pause(){
+		console.log("Paused");
+	}
+	resume(){
+		console.log("Resuming");
+	}
+}
