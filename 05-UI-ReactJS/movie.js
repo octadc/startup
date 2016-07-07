f6d068b4075a68e6eeaa8a994392abffc2c67918
Octavio Dellavedova <Octavio Dellavedova>
2016-07-07 16:52:56 -0300

    started react

---
+++ 05-UI-ReactJS/movie.js
@@ -0,0 +1,10 @@
+class movie {
+	constructor(title, year, duration, sinopsis, cover) {
+	this.title = title;
+	this.year = year;
+	this.duration = duration;
+	this.sinopsis = sinopsis;
+	this.cover = cover;
+
+	}
+}
